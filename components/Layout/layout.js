85b5c60ea630a8c6d0980fa0b992b7a782668c38
Arvind M <arvind.nexus06@gmail.com>
2022-04-05 16:15:30 +0530

    addition of layout component

---
+++ components/Layout/layout.js
@@ -0,0 +1,53 @@
+import CartIcon from "./cart.svg";
+import Input from "../Inputs/inputs";
+
+import styles from "../../styles/layout.module.scss";
+
+const Layout = ({ children }) => {
+  return (
+    <div className={styles.layoutContainer}>
+      <header className={styles.header}>
+        <div className={styles.innerContainer}>
+          <div className={styles.flexContainer}>
+            <div className={styles.flexContainer}>
+              <div className={styles.logo}>
+                <img
+                  src="https://res.cloudinary.com/dyvivj6d5/image/upload/v1648211535/logo_twd2km.svg"
+                  alt="Food Delivery"
+                />
+              </div>
+
+              <div className={styles.searchBar}>
+                <Input
+                  type="text"
+                  label=""
+                  placeholder="Search"
+                  icon="https://res.cloudinary.com/dyvivj6d5/image/upload/v1648188073/search_h2cotd.png"
+                  getValue={() => {}}
+                  isRequired={true}
+                />
+              </div>
+            </div>
+
+            <nav className={styles.navigation + " " + styles.flexContainer}>
+              <div className={styles.link}>Restaurants</div>
+              <div className={styles.link}>Deals</div>
+              <div className={styles.link}>My orders</div>
+              <div className={styles.cart}>
+                <div className={styles.items}>4</div>
+                {/* <img src={CartIcon} alt="Cart" /> */}
+              </div>
+
+              {/* <ProfileInfo /> */}
+
+              {/* <MobileNav /> */}
+            </nav>
+          </div>
+        </div>
+      </header>
+      <div className={styles.innerContainer}>{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Layout from "../components/Layout/layout";
 import Image from "next/image";
 
 export default function Home() {
@@ -10,7 +11,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>test</div>
+      <div>
+        <Layout>test</Layout>
+      </div>
     </div>
   );
 }
