1e7a98e242cb30290ce7d38d58d15b4b1ffa110f
Arvind M <arvind.nexus06@gmail.com>
2022-04-06 17:51:00 +0530

    carasoul optimization

--- components/Slider/slider.js
+++ components/Slider/slider.js
@@ -1,27 +1,12 @@
-import { useState } from "react";
 import { data } from "./data";
 import { Carousel } from "antd";
 import styles from "../../styles/slider.module.scss";
 
 export default function Slider() {
-  const [slideIndex, setSlideIndex] = useState(1);
-
-  const moveDot = (index) => {
-    setSlideIndex(index);
-  };
-
-  const contentStyle = {
-    height: "160px",
-    color: "#fff",
-    lineHeight: "160px",
-    textAlign: "center",
-    // background: "#364d79",
-  };
-
   return (
     <div className={styles.containerSlider}>
-      <Carousel>
-        {data.map((obj, index) => {
+      <Carousel autoplay effect="fade">
+        {data.map((obj) => {
           return (
             <div key={obj.id}>
               <div className="title">{obj.title}</div>
