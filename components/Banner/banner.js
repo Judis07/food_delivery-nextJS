f6fad937c022c8bb1fa59bfa373ca09788ae86ee
Arvind M <arvind.nexus06@gmail.com>
2022-04-07 18:32:58 +0530

    banner image changed to webp format

--- components/Banner/banner.js
+++ components/Banner/banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
         title="All deserts"
         discount="20"
         target="Deserty"
-        image="https://res.cloudinary.com/dyvivj6d5/image/upload/v1648460365/deserty_v8fiag.png"
+        image="https://res.cloudinary.com/dyvivj6d5/image/upload/v1649336522/deserty_v8fiag_bysib4.webp"
         color="#4E60FF"
         bgColor="#F3F4FF"
       />
@@ -16,7 +16,7 @@ const Banner = () => {
         title="Big Burgers"
         discount="50"
         target="Fooddies"
-        image="https://res.cloudinary.com/dyvivj6d5/image/upload/v1648460367/fooddies_bemtx8.png"
+        image="https://res.cloudinary.com/dyvivj6d5/image/upload/v1649336546/fooddies_bemtx8_ittfuy.webp"
         color="#FD6D22"
         bgColor="#FFF3ED"
       />
